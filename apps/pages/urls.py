c0e9a1173fe73ecf1ff9dc4f968bc3a2bae0211c
Yurii Menezes <yurythx@gmail.com>
2024-07-23 17:37:07 -0400

    Ravenna-0.1

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -3,8 +3,14 @@
 from django.urls import path 
 from pages import views
 
+
 urlpatterns = [
-    path('', views.index, name='home'), 
+
+    path('', views.index, name='index'), 
+    path('about/', views.about, name='about'), 
+    path('blog_index/', views.blog_index, name='blog-index'), 
+    path('blog_details/', views.blog_details, name='blog-details'), 
+    
 ]
 
 
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -6,3 +6,15 @@ from django.shortcuts import render
 def index(request):
    
     return render(request, 'index.html')
+
+def about(request):
+   
+    return render(request, 'about.html')
+
+def blog_index(request):
+   
+    return render(request, 'blog_index.html')
+
+def blog_details(request):
+   
+    return render(request, 'blog-details.html')
